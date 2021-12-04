c435fddc184c06d438add0ed7e3e303825301199
cmmarting <osioez@gmail.com>
2021-12-04 10:26:00 -0500

    Ya funciona mandando los datos desde el feature

--- build/resources/test/features/registerUtestChoucair.feature
+++ build/resources/test/features/registerUtestChoucair.feature
@@ -9,7 +9,11 @@ Feature: Utest User Registration
   Scenario: Register on Utest
 
     Given the Utest website we look for the Join Today registration
+      | strFirstName | strLastName |
+      |   Mario      | Gonzalez    |
     When you enter this section you fill in the required information and save
       | strFirstName | strLastName |
       |   Mario      | Gonzalez    |
     Then this stores the user to then be able to access and use the services of the platform
+      | strFirstName | strLastName |
+      |   Mario      | Gonzalez    |
--- src/main/java/co/com/choucair/certification/RegisterUtest/tasks/Register.java
+++ src/main/java/co/com/choucair/certification/RegisterUtest/tasks/Register.java
@@ -27,21 +27,20 @@ public class Register implements Task{
         actor.attemptsTo(Click.on(ChoucairRegisterPage.joinToday),
 
                 Enter.theValue(strFirstName).into(ChoucairRegisterPage.INPUT_FIRSTNAME),
-                Enter.theValue(strLastName).into(ChoucairRegisterPage.INPUT_LASTNAME),
-                //Enter.theValue("nombrePruebas").into(ChoucairRegisterPage.INPUT_FIRSTNAME),
-                //Enter.theValue("apellidoPruebas").into(ChoucairRegisterPage.INPUT_LASTNAME),
-                Enter.theValue("hansel@correo.com").into(ChoucairRegisterPage.INPUT_EMAIL),
-                SelectFromOptions.byValue("number:12").from(By.id("birthMonth")),
-                SelectFromOptions.byValue("number:2").from(By.id("birthDay")),
-                SelectFromOptions.byValue("number:1987").from(By.id("birthYear")),
+                Enter.theValue(strLastName).into(ChoucairRegisterPage.INPUT_LASTNAME)
+
+                //Enter.theValue("hansel@correo.com").into(ChoucairRegisterPage.INPUT_EMAIL),
+                //SelectFromOptions.byValue("number:12").from(By.id("birthMonth")),
+                //SelectFromOptions.byValue("number:2").from(By.id("birthDay")),
+                //SelectFromOptions.byValue("number:1987").from(By.id("birthYear")),
                 //SelectFromOptions.byValue("languages").from(By.id("Spanish")),
                 //Click.on(ChoucairRegisterPage.SELECT_LANGUAGE),
-                Click.on(ChoucairRegisterPage.NEXT_BUTTON0),
+                //Click.on(ChoucairRegisterPage.NEXT_BUTTON0),
 
-                Click.on(ChoucairRegisterPage.INPUT_CITY),
-                Enter.theValue("705030").into(ChoucairRegisterPage.INPUT_ZIP),
+                //Click.on(ChoucairRegisterPage.INPUT_CITY),
+                //Enter.theValue("705030").into(ChoucairRegisterPage.INPUT_ZIP),
                 //Enter.theValue("Colombia").into(ChoucairRegisterPage.SELECT_COUNTRY),
-                Click.on(ChoucairRegisterPage.NEXT_BUTTON1),
+                //Click.on(ChoucairRegisterPage.NEXT_BUTTON1),
 
                 //SelectFromOptions.byVisibleText("Windows").from(By.className("ui-select-match")),
                 //Click.on(ChoucairRegisterPage.INPUT_PC),
@@ -50,13 +49,13 @@ public class Register implements Task{
                 //Click.on(ChoucairRegisterPage.INPUT_MOVIL),
                 //Click.on(ChoucairRegisterPage.INPUT_MODEL),
                 //Click.on(ChoucairRegisterPage.SELECT_OS),
-                Click.on(ChoucairRegisterPage.NEXT_BUTTON2),
+                //Click.on(ChoucairRegisterPage.NEXT_BUTTON2),
 
-                Enter.theValue("Mm@123456.").into(ChoucairRegisterPage.INPUT_PASS),
-                Enter.theValue("Mm@123456.").into(ChoucairRegisterPage.INPUT_CONFIPASS),
-                Click.on(ChoucairRegisterPage.CHECK_TERM),
-                Click.on(ChoucairRegisterPage.CHECK_PRIVACY),
-                Click.on(ChoucairRegisterPage.NEXT_BUTTON3)
+                //Enter.theValue("Mm@123456.").into(ChoucairRegisterPage.INPUT_PASS),
+                //Enter.theValue("Mm@123456.").into(ChoucairRegisterPage.INPUT_CONFIPASS),
+                //Click.on(ChoucairRegisterPage.CHECK_TERM),
+                //Click.on(ChoucairRegisterPage.CHECK_PRIVACY),
+                //Click.on(ChoucairRegisterPage.NEXT_BUTTON3)
                 );
     }
 }
